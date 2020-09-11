567eed4d4a6ddef867b7fed9223c88c0c2c8bb35
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-09-11 08:46:55 -0300

    header component created

---
+++ web/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Home() {
+    return(
+        <div>
+            {/* Header Container */}
+            <div>
+                <img src="http://placehold.it/180x100" alt="Logo" />
+                <h2>Conversor</h2>
+                <h2>Como usar</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
