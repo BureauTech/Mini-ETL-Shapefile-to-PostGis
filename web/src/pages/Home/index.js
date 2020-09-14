a982e924b724df2449eae721f81198589339e8fe
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-09-13 22:52:53 -0300

    main component added

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -12,15 +12,28 @@ function Home() {
     <>
       <Header />
       <Faq />
+      
       <div className="main-container">
-        <div className="header-container">
+        <div className="shape-header">
           <p>SHAPEFILE para POSTGREE</p>
           <span> importe seus arquivos SHAPEFILE para seu banco de dados POSTGREESQL com segurança e confiabilidade.</span>
         </div>
+        
         <div className="shape-button">
-          POSTGREE para SHAPEFILE
+          <img src="http://placehold.it/520x360" alt="Shape-Button"/>
+        </div>
+
+        <div className="header-content">
+          <p>SHAPEFILE para POSTGREE</p>
+          <span> importe seus arquivos SHAPEFILE para seu banco de dados POSTGREESQL com segurança e confiabilidade.</span>
+        </div>
+        
+        <div className="shape-button">
+          <img src="http://placehold.it/520x360" alt="Shape-Button"/>
         </div>
       </div>
+
+      
     </>
   );
 }
