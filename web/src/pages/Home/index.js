dbe38a0601c93c27235608125c46c79b79129a76
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-09-13 22:23:13 -0300

    fine adjust and main container init

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -12,6 +12,15 @@ function Home() {
     <>
       <Header />
       <Faq />
+      <div className="main-container">
+        <div className="header-container">
+          <p>SHAPEFILE para POSTGREE</p>
+          <span> importe seus arquivos SHAPEFILE para seu banco de dados POSTGREESQL com segurança e confiabilidade.</span>
+        </div>
+        <div className="shape-button">
+          POSTGREE para SHAPEFILE
+        </div>
+      </div>
     </>
   );
 }
