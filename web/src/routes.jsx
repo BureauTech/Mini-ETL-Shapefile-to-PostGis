7d65776a74fcf66bd02c3cf6f2bf9419c24769c2
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-09-16 21:17:56 -0300

    route defined

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -4,11 +4,17 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 //Pages
 import Home from './pages/Home';
+import Shape from './pages/Shape';
+import Post from './pages/Postgis';
+import Guide from './pages/HowToUse';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" component={Home} exact/>
+            <Route path="/post" component={Post}/>
+            <Route path="/shape" component={Shape}/>
+            <Route path="/how-to-use" component={Guide}/>
     
         </BrowserRouter>
     )
