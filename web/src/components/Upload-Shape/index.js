c99196a2a5d0ae65502a107169b7bfb5a9e8a103
charles-ramos <charles@back4app.com>
2020-10-16 12:03:32 -0300

    botão carregar corrigido

--- web/src/components/Upload-Shape/index.js
+++ web/src/components/Upload-Shape/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Dropzone from 'react-dropzone';
 
 import shapeStep1 from '../../assets/img/shape-post-new.png';
 
 import {DropContainer, UploadMessage} from "./styles";
 
-
+import api from '../../services/api';
 function UploadShape() {
 
-    return (
-      <Dropzone accept="multipart/form-data"  onDropAccepted={() => {}}>
+    const [file, setFile] = useState();
 
-        {({getRootProps, getInputProps, isDragActive, isDragReject}) => (
+    const carregar = (() => {
+        api({
+          method: 'post',
+          url: '/upload', 
+          data: {
+              "file": file
+          } 
+        })
+        console.log('olha o file aqui: ' + file)
+      })
+    
+    return (
+        <>
+      <Dropzone accept="multipart/form-data"  onDropAccepted={event => setFile(event.target.value)}>
+
+        {({getRootProps, getInputProps, isDragActive, isDragReject}) =>  (
           <DropContainer
 
             {... getRootProps()}
@@ -24,8 +38,12 @@ function UploadShape() {
         )}
 
       </Dropzone>
+      <div>
+        <button type="button" onClick={carregar} enctype="multipart/form-data">
+            CARREGAR
+        </button>
+        </div>
+        </>
     );
-
-  }
-
+    }
 export default UploadShape;
