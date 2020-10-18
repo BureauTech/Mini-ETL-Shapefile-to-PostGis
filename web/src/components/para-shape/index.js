98a5d6a35623f03ba841f54a94f0a90cf648d706
charles-ramos <charles@back4app.com>
2020-10-18 20:34:44 -0300

    upload de arquivos

--- web/src/components/para-shape/index.js
+++ web/src/components/para-shape/index.js
@@ -4,7 +4,6 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
 
 
 import "./styles.css";
