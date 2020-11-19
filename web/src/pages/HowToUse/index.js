9d0827d88bd1f9ca1da0c156cda465e1257f1065
anaclaragraciano <acgsjc@gmail.com>
2020-11-19 10:08:48 -0300

    estilizando tela 'como usar'

--- web/src/pages/HowToUse/index.js
+++ web/src/pages/HowToUse/index.js
@@ -5,24 +5,33 @@ import {Link} from 'react-router-dom';
 import Background from '../../assets/img/coming-soon.jpg';
 import Logo from '../../assets/img/logo.png';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Faq from '../../components/Faq';
 import "./styles.css";
 
 const Guide = () => {
     return (
-        <div id="main-container">
-            <header>
-                <Link to="/">
-                    <FiArrowLeft />
-                    Voltar para página inicial
-                </Link>
-            </header>
-        
-            <div className="spoiler-image">
-                <img src={Logo} alt="Logo" />
-                <img src={Background} alt="Spoiler-Alert"/>
-            </div>
+<>
+      <Header />
+      <Faq />
+      
+      <div className="main-container">
+        <div className="shape-header">
+          <p>MANUAL DO USUÁRIO</p>
+          <span>Nosso guia para ajudar você a usar a ferramenta ShapeGIS de maneira mais eficaz.</span>
         </div>
+        
+        
+        <div className="post-header">
+          <p>POSTGIS para SHAPEFILE</p>
+          <span>Gere arquivos SHAPEFILE do seu banco de dados POSTGRESQL com segurança e confiabilidade.</span>
+        </div>
+
+      </div>
+
+      <Footer />    
+    </>
     );
 }
 
